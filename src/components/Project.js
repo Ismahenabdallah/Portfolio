ed8626089d49d7fd96f54e8dd071e8ebec95a54a
Ismahenabdallah <abdallah.ismahen99@gmail.com>
2023-05-31 13:14:39 +0200

    design projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,14 @@
-import React from 'react';
-
 const Project = ({ item }) => {
   return (
-    <a href={`${item.link}`} key={item.id} className='flex flex-col items-center text-center'>
-      <div className='mb-8'>
-        <img className='rounded-2xl' src={item.image} alt='' />
+    <a href={`${item.link}`} key={item.id} className='flex flex-col items-center text-center  rounded-lg p-4 shadow-lg transition duration-300 hover:shadow-xl '>
+      <div className='mb-4'>
+        <img className='rounded-lg ' src={item.image} alt='' />
       </div>
-      <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
-      <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
+      <p className='uppercase text-accent text-sm mb-2'>{item.category}</p>
+      <h3 className='text-2xl font-semibold capitalize mb-2'>{item.name}</h3>
       <div className='text-base max-w-md'>
-        #{item.language}
-        {item.name==="smartdelivery" ?<h1 className='text-gray-300'>#Project_Fin_D'etude</h1> : null}
+        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2'>{item.language}</span>
+        {item.name === 'smartdelivery' ? <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>Project_Fin_D'etude</span> : null}
       </div>
     </a>
   );
